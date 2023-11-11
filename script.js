73f7a52ed62cd19364e9351f44f448bc1cbc61f3
Rasik Abdullah <rasikabdullah@gmail.com>
2023-11-11 20:54:53 +0600

    js added

---
+++ script.js
@@ -0,0 +1,24 @@
+let secretNumber = Math.floor(Math.random() * 100) + 1;
+let attempts = 0;
+
+function checkGuess() {
+  const userGuess = document.getElementById('userGuess').value;
+  attempts++;
+
+  if (userGuess == secretNumber) {
+    document.getElementById('message').innerHTML = `alert('Congratulations! You guessed the number in ${attempts} attempts.')`;
+    document.getElementById('range').innerHTML = `The correct number was ${secretNumber}.`;
+  } else if (userGuess < secretNumber) {
+    document.getElementById('message').innerHTML = 'Too low. Try again.';
+  } else {
+    document.getElementById('message').innerHTML = 'Too high. Try again.';
+  }
+}
+
+function resetGame() {
+  secretNumber = Math.floor(Math.random() * 100) + 1;
+  attempts = 0;
+  document.getElementById('message').innerHTML = '';
+  document.getElementById('range').innerHTML = 'Enter a number between 1 and 100:';
+  document.getElementById('userGuess').value = '';
+}
